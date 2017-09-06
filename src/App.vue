7e699620dacca178928e48336c3e179a05bd3110
Holly <holly_dysania@sina.com>
2017-09-06 16:42:32 +0800

    初始化完成

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <router-view></router-view> -->
+    <div>
+      <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div
+    <div class="content">
+      content
+    </div>
+  </div>
+    </div>
+</template>
+
+<script>
+import header from './components/header/header.vue'
+export default {
+  name: 'app',
+  components:{
+    'v-header':header
+  }
+}
+</script>
+
+<style lang="stylus"  rel="stylesheet/stylus">
+  #app
+    .tab
+      display flex
+      width 100%
+      height 40px
+      line-height 40px
+      .tab-item
+        flex:1
+        text-align center
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id='header'>
+      header
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+    };
+  }
+}
+</script>
+<style lang="stylus">
+</style>
